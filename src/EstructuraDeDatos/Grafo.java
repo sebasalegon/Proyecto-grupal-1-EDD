01f738674f866b831a59d3d6600170d3b5f527d8
sebasalegon <167771363+sebasalegon@users.noreply.github.com>
2024-10-24 00:47:58 -0400

    Funcionalidad del grafo

--- src/EstructuraDeDatos/Grafo.java
+++ src/EstructuraDeDatos/Grafo.java
@@ -47,5 +47,40 @@ public class Grafo {
         return null;
     }
     
+    public void newEdge(String a, String b){
+        Nodo estA = this.searchStation(a);
+        Nodo estB = this.searchStation(b);
+        
+        if(estA != estB && estA != null && estB != null){
+            estA.getNearStations().insert(b);
+            estB.getNeatStations().insert(a);
+        }
+    }
+    
+    public void auxDFS(int v, boolean[] visitados, int contador){
+        visitados[v] = true;
+        System.out.println(v);
+        if(contador != getT()){
+            for (int=0; i < this.getMax_vertex(); i++){
+                if((v !=i)&&(!visitados[i])&&this.getStations()[i].getNearStations().search(this.getStations()[v].getStation()) !=null){
+                    auxDFS(i, visitados, contador + 1);
+                    
+                }
+            }
+        }
+    }
+    
+    public void dfs(){
+        boolean visitados[] = new boolean[this.getMax_vertex()];
+        
+        for (int i=0; i < this.getMax_vertex(); i++){
+            visitados[i] = false;
+        }
+        for (int i=0; i<this.getMax_vertex(); i++){
+            if(!visitados[i]){
+                auxDFS(i, visitados, contador:0);
+            }
+        }
+    }
 }
     
