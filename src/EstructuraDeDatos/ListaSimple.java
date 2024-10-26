7fe659532544bfda2e8815a142ab6e256a5dcb93
sebasalegon <167771363+sebasalegon@users.noreply.github.com>
2024-10-25 20:44:43 -0400

    Hola soy jesus desde la computadora de sebastian mi computadora tiene un problema para comprimir los archivos y montar los puush asi que pase completo la clase de la lista por aqui ya es funcional, igual revisen cuando vayan a actualizar lo demas.

--- src/EstructuraDeDatos/ListaSimple.java
+++ src/EstructuraDeDatos/ListaSimple.java
@@ -6,16 +6,108 @@ package EstructuraDeDatos;
 
 /**
  *
- * @author sebas
+ * @author jesus 
  */
 public class ListaSimple {
 
-    void insert(String b) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private Nodo first;
+    private Nodo last;
+    private int size;
+
+    public ListaSimple() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+
     }
 
-    Object search(String station) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void insert(String station) {
+        Nodo newNode = new Nodo(station);
+        if (this.getFirst() == null) {
+            this.setFirst(this.last = newNode);
+        } else {
+            this.getLast().setNext(newNode);
+            this.setLast(this.getLast().getNext());
+        }
+        this.setSize(this.getSize() + 1);
     }
-    
+
+    public Nodo search(String station) {
+        station = station.toLowerCase();
+        String name[] = station.split("_");
+        if (this.getFirst() == null) {
+            return null;
+        } else {
+            Nodo aux = this.getFirst();
+            if (name.length <= 1) {
+                while (aux != null && !aux.getStation().toLowerCase().equals(station)) {
+                    aux = aux.getNext();
+                }
+                return aux;
+            } else {
+                while (aux != null && !aux.getStation().toLowerCase().contains(name[0])) {
+                    aux = aux.getNext();
+                }
+                return aux;
+            }
+        }
+    }
+
+    public String print() {
+        if (this.getFirst() == null) {
+            return "NO HAY ESTACIONES CERCANAS";
+        } else {
+            Nodo aux = this.getFirst();
+            String print = "";
+            while (aux != null) {
+                if (!aux.getStation().equals("")) {
+                    print += aux.getStation() + ", ";
+                }
+                aux = aux.getNext();
+            }
+            return print;
+        }
+    }
+
+    /**
+     * @return the first
+     */
+    public Nodo getFirst() {
+        return first;
+    }
+
+    /**
+     * @param first the first to set
+     */
+    public void setFirst(Nodo first) {
+        this.first = first;
+    }
+
+    /**
+     * @return the last
+     */
+    public Nodo getLast() {
+        return last;
+    }
+
+    /**
+     * @param last the last to set
+     */
+    public void setLast(Nodo last) {
+        this.last = last;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
